916d5bf9848944eaf88843ea24ef496aff53aac2
diego marin <dilema4000@gmail.com>
2022-09-09 14:20:10 -0500

    Challenge Rails Task Manager2

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,19 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :tasks
+  # Read
+  #get "tasks", to: "tasks#index", as: :tasks
+  #get "tasks/id", to: "tasks#show", as: :task
+
+  # Create
+  #get "tasks/new", to: "tasks#new", as: :new
+  #post "restaurants", to: "restaurants#create"
+
+  # Update
+  #get "tasks/:id/edit", to: "tasks#edit"
+  #patch "restaurants/:id", to: "tasks#update"
+
+  # Delete
+  #delete "tasks/:id", to: "tasks#destroy"
 end
